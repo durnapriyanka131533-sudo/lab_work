e9b6354a3b39dff6b190b381574bd5a1cc289f21
durnapriyanka131533-sudo <durna.priyanka131533@marwadiuniversity.ac.in>
2026-06-22 09:44:40 +0530

    Add files via upload

---
+++ p2.php
@@ -0,0 +1,14 @@
+<?php
+$num1 = 25;
+$num2 = 70;
+$num3 = 45;
+
+// Maximum number
+$max = max($num1, $num2, $num3);
+
+// Minimum number
+$min = min($num1, $num2, $num3);
+
+echo "Maximum Number: " . $max . "<br>";
+echo "Minimum Number: " . $min;
+?>
