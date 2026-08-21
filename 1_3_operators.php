62b6e427b3840c81333736a221e3058c9dd50b60
durnapriyanka131533-sudo <durna.priyanka131533@marwadiuniversity.ac.in>
2026-08-21 09:36:21 +0530

    Add files via upload

---
+++ 1_3_operators.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 20;
+$b = 10;
+
+echo "Addition: " . ($a + $b) . "<br>";
+echo "Subtraction: " . ($a - $b) . "<br>";
+echo "Multiplication: " . ($a * $b) . "<br>";
+echo "Division: " . ($a / $b) . "<br>";
+echo "Modulus: " . ($a % $b) . "<br>";
+
+echo "Greater than: ";
+var_dump($a > $b);
+
+echo "<br>Equal to: ";
+var_dump($a == $b);
+
+?>
